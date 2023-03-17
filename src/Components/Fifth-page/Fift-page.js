326bd1a013b732d19ddf4d37c5603dda8e2b425b
pathanimran4373 <pathanimran4373@gmail.com>
2023-03-17 19:13:36 +0530

    portfoluio

---
+++ src/Components/Fifth-page/Fift-page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Fifth-page.css'
+function FifthPage() {
+  return (
+    <div>
+      <div class="project" id="project">
+        <h1 class="heading">my projects </h1>
+        <div class="lag">
+          <ul>
+            <li><a href="https://pathanimran4373.github.io/fitnesswebsite.github.io/
+                       " target="_blank">website for gym <br /><p>[ React, scss ]</p></a></li>
+            <li><a href=" https://pathanimran4373.github.io/carwebsite.github.io/" target="_blank">website for car shop <br /><p>[ HTML, CSS, Bootstrap ]</p></a></li>
+
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default FifthPage
